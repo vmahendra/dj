092976774ffbe2b14c0cdeea350c01ca0a13c627
vmahendra <vijaymahendra85@gmail.com>
2014-09-05 16:52:57 +0530

    project

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,14 @@
+class WelcomeController < ApplicationController
+  def index
+  end
+  def dropdown1
+  end
+  def dropdown2
+  end
+  def dropdown3
+  end
+  def section3
+  end
+  def section4
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dj::Application.config.secret_token = 'fbc82946321eba7082f3ac647e370f083c317f4029e4540e800fe4bb409e5560cc64dfa081a0f86e54f5016aeae6d55a5d79c6816ac89499998a34b2f79d3abc'
